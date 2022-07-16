d30dc0114210c0e23fa281b90fa755a23ef8c8ba
martinsspn <47855595+martinsspn@users.noreply.github.com>
2022-07-16 00:17:08 -0300

    Projeto Fiware

---
+++ ProjetoFiware/src/main/java/br/ufrn/Subscribe/Controller/SubscribeController.java
@@ -0,0 +1,66 @@
+package br.ufrn.Subscribe.Controller;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/subscribe")
+public class SubscribeController {
+
+
+	public static String createSubscription(String id){
+		JSONObject subscription = new JSONObject();
+		List<JSONObject> entities = new ArrayList<>();
+		JSONObject json = new JSONObject();
+		json.put("isPattern", "false");
+		json.put("id", id);
+		json.put("type", "Deposito_de_lixo");
+		entities.add(json);
+		subscription.put("entities", entities);
+		List<String> attributes = new ArrayList<>();
+		attributes.add("Nivel_qtd_lixo");
+		subscription.put("attributes", attributes);
+		subscription.put("reference", "http://localhost:8080/Subscribe/subscribe");
+		subscription.put("duration", "P1M");
+		List<JSONObject> notifyConditions = new ArrayList<>();
+		JSONObject type = new JSONObject();
+		type.put("type", "ONCHANGE");
+		notifyConditions.add(type);
+		subscription.put("notifyConditions", notifyConditions);
+		subscription.put("throttling", "PT5S");
+		return subscribe(subscription);
+	}
+
+	public static String subscribe(JSONObject subscription){
+		String url = "http://127.0.0.1:1026/v1/subscribeContext";
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> httpEntity = new HttpEntity<>(subscription.toString(), httpHeaders);
+		RestTemplate restTemplate = new RestTemplate();
+
+		return restTemplate.postForObject(url, httpEntity, String.class);
+
+	}
+
+	@PostMapping
+	public void exibirSubscricao(@RequestBody JsonNode body) {
+
+		System.out.println("\n\n\n"+body.toPrettyString());
+		
+	}
+
+}
---
+++ ProjetoFiware/src/main/java/br/ufrn/Subscribe/SubscribeApplication.java
@@ -0,0 +1,33 @@
+package br.ufrn.Subscribe;
+
+import br.ufrn.Subscribe.Controller.PublishController;
+import br.ufrn.Subscribe.Controller.SubscribeController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class SubscribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscribeApplication.class, args);
+        while (true) {
+            System.out.println("Escolha a opção:");
+            System.out.println("1 - Adicionar novo deposito de lixo");
+            System.out.println("2 - Subscrever em um deposito de lixo");
+            System.out.print("> ");
+            Scanner leitor = new Scanner(System.in);
+            int op = leitor.nextInt();
+            switch (op) {
+                case 1 -> System.out.println(PublishController.createContext());
+                case 2 -> {
+                    System.out.println("informe o id do deposito:");
+                    String idDeposito = leitor.next();
+                    System.out.println(SubscribeController.createSubscription(idDeposito));
+                }
+                default -> System.out.println("Opcao invalida");
+            }
+        }
+    }
+}
